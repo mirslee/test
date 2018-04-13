01f6468963ded6ab9d19ee1e31128cda37d07c4c
mirs_lee@163.com <907398237@qq.com>
2018-04-14 01:15:25 +0800

    111

--- GLTest/GLTest/GLTest.cpp
+++ GLTest/GLTest/GLTest.cpp
@@ -18,6 +18,18 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
+
+	/*glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans;
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans*vec;
+	std::cout << vec.x << "	" << vec.y << "	" << vec.z << "	" << std::endl;*/
+
+	glm::mat4 trans;
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+
+
 	// glfw: initialize and configure
 	// ------------------------------
 	glfwInit();
--- GLTest/GLTest/stdafx.h
+++ GLTest/GLTest/stdafx.h
@@ -12,6 +12,9 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <string>
 #include <fstream>
